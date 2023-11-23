2e35c8dced8351ef0c02c7fde79e5625ee18795a
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-11-24 00:39:51 +0530

    feat: first stable commit

---
+++ assistant.ts
@@ -0,0 +1,57 @@
+
+import { OpenAI } from "openai";
+
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const initializeThread = async () => {
+  const thread = await openai.beta.threads.create();
+  console.debug("[THREAD]", thread)
+  return thread;
+}
+
+
+export const sendMessage = async (threadId: string, message: string) => {
+
+  const newMessage = await openai.beta.threads.messages.create(threadId, {
+    role: 'user',
+    content: message,
+  })
+  console.debug("[APPENDED_THREAD]", newMessage)
+
+
+  const assistant = await openai.beta.assistants.retrieve("asst_UriK8p1GRkSRLKKGm5Dyuync", {
+    stream: false,
+    maxRetries: 3,
+  });
+  console.debug("[ASSISTANT]", assistant)
+
+
+  const run_response = await openai.beta.threads.runs.create(threadId, {
+    assistant_id: assistant.id,
+  })
+  console.debug("[RUN_RESPONSE]", run_response)
+
+
+  let retrieved_runs = await openai.beta.threads.runs.retrieve(threadId, run_response.id);
+  while (retrieved_runs.status !== 'completed') {
+    await new Promise(resolve => setTimeout(resolve, 600));
+    retrieved_runs = await openai.beta.threads.runs.retrieve(threadId, run_response.id);
+  }
+  console.debug("[RETRIEVED_RUNS]", retrieved_runs)
+
+
+  const messages = await ListMessages(threadId);
+  // const lastMessage = messages.find((message) => message.run_id === run_response.id && message.role === 'assistant');
+  // console.debug("[LAST_MESSAGE]", lastMessage)
+
+
+  return messages;
+}
+
+export const ListMessages = async (threadId: string) => {
+  const messages = await openai.beta.threads.messages.list(threadId);
+  console.debug("[MESSAGES]", messages);
+  return messages.data;
+}
---
+++ index.ts
@@ -0,0 +1,90 @@
+declare var require: any
+
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+
+import { Server } from 'socket.io';
+import { HfInference } from '@huggingface/inference'
+import { ListMessages, initializeThread, sendMessage } from './assistant';
+import { Thread } from 'openai/resources/beta/threads/threads';
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+})
+
+
+type UserSession = {
+  user_id: string,
+  sessions: string[],
+  user: {
+    name: string,
+    email: string,
+    id: string
+  },
+  thread: Thread
+}
+
+
+const user_sessions: UserSession[] = [];
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('user_connected', async (user) => {
+    console.log("[USER_CONNECTED]", user);
+
+    let userSession: UserSession | null = null;
+
+    const existing_user_session = user_sessions.find((user_session) => user_session.user_id === user?.email)
+
+    if (existing_user_session) {
+      console.debug("[EXISTING_USER_SESSION]", existing_user_session)
+      const existing_user_session_socket = existing_user_session.sessions.find((session) => session === socket.id)
+      if (!existing_user_session_socket) {
+        existing_user_session.sessions.push(socket.id);
+      }
+      userSession = existing_user_session;
+    } else {
+      console.debug("[NEW_USER_SESSION]")
+      console.log("[USER_CONNECTED]", user);
+
+      const thread = await initializeThread();
+      const user_session: any = {};
+
+      user_session.user_id = user?.email;
+      user_session.sessions = [socket.id];
+      user_session.user = user;
+      user_session.thread = thread;
+
+      console.log("[USER_SESSION]", user_session)
+
+      user_sessions.push(user_session);
+      userSession = user_session;
+    }
+
+    const messages = await ListMessages((userSession?.thread?.id as string));
+    socket.emit('user_connection_success', { userSession, messages: messages });
+
+  })
+
+
+  socket.on('message', async (data: { message: string, userSession: UserSession }) => {
+
+    console.log("[MESSAGE_DATA]", data);
+
+    // OPENAI
+    const response = await sendMessage(data.userSession?.thread?.id, data.message);
+    console.log("[GPT_RESPONSE]", response);
+    socket.emit('conversation_response', response);
+
+  })
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+})
